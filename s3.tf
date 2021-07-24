fbfdd587be40aff2cd9e8bf8e30ede22f9e6b903
Cloud-42 <hello@cloud42.io>
2021-07-24 19:03:02 +0000

    Fix s3 name

--- s3.tf
+++ s3.tf
@@ -1,23 +1,18 @@
 # ---------------------------------------
 # s3 Bucket
 # ---------------------------------------
-
 resource "aws_s3_bucket" "data" {
-  bucket_prefix = "${var.environment}-${aws_codedeploy_app.app.name}-"
+  bucket = "${var.name}-${aws_codedeploy_app.app.name}-"
   acl           = "private"
   versioning {
     enabled = "true"
   }
-  tags = {
-    Environment   = var.environment
-    orchestration = var.orchestration
-    purpose       = "${var.environment}-${aws_codedeploy_app.app.name}-data"
-  }
+  tags = var.tags
   lifecycle_rule {
     enabled = var.lifecycle_rule_enabled
 
     expiration {
-      days = 120
+      days = var.expiration
     }
   }
 }
