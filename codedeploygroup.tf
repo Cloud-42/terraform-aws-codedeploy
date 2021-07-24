279c251ae2b1a71f7827799fec0e7e1c15295456
Cloud-42 <hello@cloud42.io>
2021-07-24 19:05:29 +0000

    Update name

--- codedeploygroup.tf
+++ codedeploygroup.tf
@@ -3,9 +3,8 @@
 #
 resource "aws_codedeploy_deployment_group" "this" {
   app_name               = aws_codedeploy_app.app.name
-  deployment_group_name  = "${var.environment}-${aws_codedeploy_app.app.name}-deployment-group"
+  deployment_group_name  = "${aws_codedeploy_app.app.name}-deployment-group"
   deployment_config_name = var.deployment_config_name
   service_role_arn       = aws_iam_role.role.arn
   autoscaling_groups     = [var.autoscaling_groups]
 }
-
